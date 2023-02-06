79865a47716f4111bf65dca312e11570e0aa8035
Eliot <sdetechnologiesllc@gmail.com>
2023-02-06 14:13:16 -0500

    Added file uploads

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState, useEffect } from 'react'; 
 import React, { Component} from 'react';
 import {Helmet} from 'react-helmet'
+import axios from 'axios';
 // import 'SpotifyAPIHandler.js';
 
 class App extends Component {
@@ -18,18 +19,113 @@ class App extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount(){
+  asynccomponentDidMount(){
 
     console.log('component mounted');
 
   }
 
+  onFileChange = event => {
+    this.setState({selectedFiles: event.target.files});
+  }
+
+  onFileUpload = async () => {
+    const formData = new FormData();
+
+    var newFileList = [];
+
+    console.log('length: ', this.state.selectedFiles.length);
+
+    // for(var i = 0; i < this.state.selectedFiles.length; i++){
+    //   var tempFile = this.state.selectedFiles[i];
+    //   // var tempFileData = {
+    //   //   'name': tempFile.name,
+    //   //   'data': tempFile.data,
+    //   //   'size': tempFile.size,
+    //   //   'mv': Function(tempFile.mv),
+    //   // }
+
+    //   // console.log('tempFileData: ', tempFileData);
+    //   console.log('tempFile(',i, '):', tempFile);
+    //   newFileList.push(tempFile);
+    // }
+
+    // console.log('newFileList: ', newFileList);
+
+    for(var i = 0; i < this.state.selectedFiles.length; i++){
+      formData.append('FileList', this.state.selectedFiles[i]);
+    }
+
+    // console.log('this.state.selectedFiles[0]: ', this.state.selectedFiles[0]);
+    // formData.append('fileName', this.state.selectedFile.name);
+
+    console.log('details of the uploaded file: ', this.state.selectedFiles);
+    // console.log('formData: ', formData);
+
+    // axios.post("/files", formData);
+
+    // const response = fetch('http://localhost:5000/files', {
+    //   method: 'POST',
+    //   headers: {
+    //     // 'Accept': 'application/json',
+    //     'content-Type': 'multipart/form-data',
+    //     // 'Content-Type': 'file.type',
+    //   },
+    //   body: formData,
+    // })
+    // .then(res => console.log(res))
+    // .then(data => console.log('Success: ', data))
+    // .catch(error => console.log('Error: ', error));
+
+    const config = {
+
+      headers: {
+
+        'content-type': 'multipart/form-data',
+
+      },
+
+    };
+
+    await axios.post('http://localhost:5000/files', formData, config)
+    .then((response) => {
+      console.log('response: ', response.data);
+    })
+    .catch(error => console.log('error: ', error))
+    // console.log('response: ', response);
+
+  }
+
+  fileData = () => {
+    if(this.state.selectedFile){
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p> File Name: {this.state.selectedFile.name}</p>
+          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>
+            Last Modified: {" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  }
 
   handleChange(e) {
     console.log('handleChange: ', e);
 
     if(e.target.type == "file"){
       this.setState({[e.target.name]: e.target.files}, () => console.log('new state: ', this.state));
+      this.setState({selectedFiles: e.target.files}, () => console.log('new state: ', this.state))
     } else {
       this.setState({[e.target.name]: e.target.value}, () => console.log('new state: ', this.state));
     }
@@ -43,36 +139,76 @@ class App extends Component {
     e.preventDefault();
 
     if(e.target.name == "submit_button"){
-      var result = await hitCreateVideo(this.state);
+      var result = await this.hitCreateVideo();
 
       if(result == 0){
         console.log('Success.');
         this.setState({data_sent: true}, () => console.log('new state: ', this.state));
         return;
-      }
-
-      if(result == 1){
+      } else if(result == 1){
         console.log('Empty field');
+        return;
+      } else if(result == 2){
+        console.log('Only one file was selected. Select at least one more');
+        return;
       }
-
+      
+      // await testUploadVideo(this.state.video_files);
+      // this.onFileUpload();
     }
+  }
+
+  hitCreateVideo = async () => {
+
+  
+    console.log('hitCreateVideo with state" ', this.state);
+  
+    var {data_sent, video_files, spotify_url} = this.state;
+  
+    if(video_files == '' || spotify_url == ''){
+      return 1;
+    }
+  
+    if(video_files.length < 2){
+      return 2;
+    }
+  
+    const response = await fetch('http://localhost:5000/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.log('Error: ', error))
+      // console.log('response: ', res.);
+      // return response;
+  
+    this.onFileUpload();
+  
+    this.setState({data_sent: true}, () => console.log('confirmed data sent. new state: ', this.state))
+
+    return 0;
   }
 
   render(){
 
-    const response = fetch('http://localhost:5000/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.state),
-  })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.log('Error: ', error))
+    
+  //   const response = fetch('http://localhost:5000/api', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(this.state),
+  // })
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  //   .catch(error => console.log('Error: ', error))
 
-    var {data_sent, video_files, spotify_url} = this.state;
-    // console.log('current state: ', this.state);
+  //   var {data_sent, video_files, spotify_url} = this.state;
+  //   // console.log('current state: ', this.state);
 
     return (
       <div className="App">
@@ -83,17 +219,24 @@ class App extends Component {
           <div className="card rounded-3 border-3 shadow-lg p-4 m-4 w-40" style={{width: '18rem'}}>
           <div className="card-body shadow-lg">
         <div id="input">
-          <form action="/api" method="POST">
+          {/* <form action="/api" method="POST"> */}
+          <form>
             Enter spotify song URL: 
             <input type="text" name = "spotify_url" onChange={this.handleChange}></input>
             Upload video files: 
-            <input type="file" multiple accept='application/pdf, image/png' name = "video_files" onChange={this.handleChange}></input>
-            <button className="btn btn-primary" type="submit" name = "submit_button" onClick={this.handleSubmit}>Create Video</button>
+            <input type="file" multiple accept='image/png, image/jpg, video/mp4' name = "video_files" onChange={this.handleChange}></input>
+            {/* <button className="btn btn-primary" type="submit" name = "submit_button" onClick={this.handleSubmit}>Create Video</button> */}
+            {/* <input type="file" onChange={this.onFileChange} /> */}
+            {/* <button className="btn btn-primary" type="submit" name = "submit_button" onClick={this.onFileUpload}>Create Video</button> */}
+            {/* <input type="file" onChange={this.handleChange}/> */}
+            <button type="submit" name="submit_button" onClick={this.handleSubmit}>Upload</button>
           </form>
         </div>
         </div>
+        {/* {this.fileData()} */}
         </div>
         </div>
+        
           
       </div>
 
@@ -107,6 +250,34 @@ class App extends Component {
   
 }
 
+async function testUploadVideo(tempFile){
+  console.log('testUploadVideo with tempFile: ', tempFile);
+
+  let formData = new FormData();
+  formData.append('File', tempFile, tempFile.name);
+
+  console.log('tempFile.name: ', tempFile.name);
+
+  console.log('formData: ', formData);
+
+  // const testResponse = await fetch('http://localhost:5000/files', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'multipart/form-data',
+  //     },
+  //     body: formData,
+  // })
+  //   .then(res => res.json())
+  //   .then(data => console.log('Success: ', data))
+  //   .catch(error => console.log('Error: ', error))
+
+  // console.log('testResponse: ', testResponse);
+
+  axios.post("http://localhost:5000/files", formData);
+
+}
+
 async function hitCreateVideo(state){
 
   
@@ -116,6 +287,10 @@ async function hitCreateVideo(state){
 
   if(video_files == '' || spotify_url == ''){
     return 1;
+  }
+
+  if(video_files.length < 2){
+    return 2;
   }
 
   const response = await fetch('http://localhost:5000/api', {
@@ -130,6 +305,8 @@ async function hitCreateVideo(state){
     .catch(error => console.log('Error: ', error))
     // console.log('response: ', res.);
     // return response;
+
+  this.onFileUpload();
 
   return 0;
 }
--- src/config/server.js
+++ src/config/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require('cors');
 const router = express.Router;
 const request = require('request');
+const fileUpload = require('express-fileupload')
+var multer = require('multer')
+var path = require('path');
+var os = require('os');
 const app = express();
 
 const port = 5000;
@@ -11,6 +15,20 @@ app.listen(port);
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended: true}))
+// app.use(fileUpload())
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/test/'
+}));
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, 'public');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+})
 
 app.get('/api', (req, res) => {
     console.log('GET request on /api');
@@ -22,18 +40,24 @@ app.post('/api', (req, res) => {
     console.log('req body: ', req.body);
     res.json(req.body);
     stateData = req.body;
-    createVideo(req.body);
+    // createVideo(req.body);
 })
 
-app.get('/files', (req, res) => {
+app.get('/files', async (req, res) => {
     console.log('GET request on /files');
-    // console.log('response: ', res.json());
-    res.json(fileFormData);
+    // await downloadFiles(req, res);
+    await res.json(fileData);
 })
 
-app.post('/files', (req, res) => {
+app.post('/files', async (req, res) => {
     console.log('POST request on /files');
     console.log('request body: ', req.body);
+    console.log('request files: ', req.files);
+    // fileData = req.files;
+    // console.log('fileData: ', fileData);
+    // console.log('response: ', res);
+    downloadFiles(req, res);
+    
 })
 
 var stateData = {
@@ -44,31 +68,66 @@ var stateData = {
 
 var videoFileList = [];
 var fileFormData = new FormData();
+var fileData = {
+    video_files: [],
+}
+tempFileData = null;
+
+function downloadFiles(req, res){
+    var filePathList = [];
+    tempFileData = req.files;
+    // console.log('fileData.FileList.length: ', fileData.FileList.length);
+    console.log('tempFileData: ', tempFileData.FileList.length);
+    if(tempFileData != null){
+        for(var i = 0; i < tempFileData.FileList.length; i++){
+            var tempFile = tempFileData.FileList[i];
+            console.log('fileData.FileList[',i,']: ', tempFile);
+            var uploadPath = './test/' + tempFile.name;
+            filePathList.push(path.join(process.cwd(),'test',tempFile.name));
+            tempFile.mv(uploadPath, function(err) {
+                if(err){
+                    console.log('error: ', err);
+                    // return res.status(500).send(err);
+                }
+            });
+            // tempFile.mv(uploadPath);
+        }
+    }
+    // res.json({
+    //     video_files: filePathList,
+    // });
+    fileData = {
+        video_files: filePathList,
+    }
+}
 
 async function uploadVideos(videoFiles){
 
     console.log('uploadVideos() with videoFiles: ', videoFiles);
 
+    // console.log('videoFiles.name: ', videoFiles.name);
+
     const formData = new FormData();
 
-    formData.append('File', videoFiles);
+    formData.append('File', videoFiles.name);
     // videoFileList =(videoFiles);
 
     console.log('formData: ', formData);
     fileFormData = formData;
 
-    await fetch('http://localhost:5000/files', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'multipart/form-data'
-        },
-        body: formData,
-    })
-        .then(res => res.json())
-        .then(data => console.log('Success: ', data))
-        .catch(error => console.log('Error: ', error))
-        console.log('res.json(): ', res.json());
+    // await fetch('http://localhost:5000/files', {
+    //     method: 'POST',
+    //     headers: {
+    //         // 'Accept': 'application/json',
+    //         // 'Content-Type': 'multipart/form-data'
+    //         'Content-Type': file.type,
+    //     },
+    //     body: formData,
+    // })
+    //     .then(res => res.json())
+    //     .then(data => console.log('Success: ', data))
+    //     .catch(error => console.log('Error: ', error))
+    //     console.log('res.json(): ', res.json());
 }
 
 function createVideo(reqData){
@@ -83,5 +142,5 @@ function createVideo(reqData){
     //     return;
     // }
 
-    uploadVideos(videoFiles)
+    // uploadVideos(videoFiles)
 }
